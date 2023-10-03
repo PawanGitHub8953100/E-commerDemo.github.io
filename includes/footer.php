76abdbf73b923dd5de046b68685c47cd64e498a5
PawanGitHub8953100 <ss8953100@gmail.com>
2023-10-03 14:36:51 +0530

    aonle Demo

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<div align="center" class="footer">
+  <table width="481" height="27" border="1" style="position:relative;top:500px;">
+    <tr>
+      <td width="314">This site is &amp;copy and &amp;Tradmark To <a href="#">e-commers</a></td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE>
+<html>
+<head><title>e-commers</title>
+<link rel="stylesheet" href="css/header.css">
+<script src="js/ecommerce.js"></script>
+</head>
+<body>
+
+<div class="ecom_container">
+ 	<header class="header">
+		<div class="logo">
+            <img src="images/ecom-logo.png" alt="Logo">
+
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Search E-Commerce">
+            <div class="icon">
+                <span>&#128269; </span>
+            </div>
+            
+        </div>
+        <span class="link-a"><a id="ank" href="#">Become a Seller</a></span>
+        <span class="cart-logo"><img id="imgl" src="images/cartlogo.jpg" alt=""></span>
+        <span class="login-logo"><a href="#"><img id="imglogin" src="images/login.png" alt=""></a></span>
+        <span class="link-cart-text"><a id="ank" href="#">Cart</a></span>
+
+            </div>
+	</header> 
+    <div class="table-div">
+        <table id="table">
+            <tr class="item-container">
+                <td><a href="#">Electronics</a></td>
+                <td><a href="#">Fashion</a></td>
+                <td><a href="#">Home & Kitchen</a></td>
+                <td><a href="#">Women</a></td>
+                <td><a href="#">Men</a></td>
+                <td><a href="#">Mobiles</a></td>
+                <td><a href="#">Bags & Footwear</a></td>
+                <td><a href="#">Kids</a></td>
+            </tr>
+        </table>
+    </div>
